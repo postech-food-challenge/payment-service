9941731f9876ac5dca5bab865090ae1c665ba08c
rafael.roque <rafael.roque@ifood.com.br>
2024-05-17 21:32:07 -0300

    fix applicationContext

--- src/main/kotlin/br/com/fiap/postech/payment_service/Application.kt
+++ src/main/kotlin/br/com/fiap/postech/payment_service/Application.kt
@@ -1,10 +1,15 @@
 package br.com.fiap.postech.payment_service
 
+import br.com.fiap.postech.payment_service.application.gateways.PaymentGateway
+import br.com.fiap.postech.payment_service.application.usecases.CreatePaymentInteract
+import br.com.fiap.postech.payment_service.application.usecases.UpdatePaymentInteract
 import br.com.fiap.postech.payment_service.infrastructure.persistance.repository.DatabaseSingleton
 import br.com.fiap.postech.payment_service.infrastructure.persistance.repository.PaymentRepository
 import br.com.fiap.postech.payment_service.infrastructure.persistance.repository.PaymentRepositoryImpl
 import br.com.fiap.postech.payment_service.plugins.configureSerialization
 import br.com.fiap.postech.payment_service.infrastructure.controller.configurePaymentController
+import br.com.fiap.postech.payment_service.infrastructure.gateways.MercadoPagoClientGateway
+import br.com.fiap.postech.payment_service.infrastructure.gateways.PaymentRepositoryGateway
 import io.ktor.server.application.*
 import kotlinx.coroutines.runBlocking
 
@@ -15,7 +20,12 @@ fun main(args: Array<String>) {
 fun Application.module() {
     DatabaseSingleton.init()
 
-    val repository: PaymentRepository = PaymentRepositoryImpl()
+    val mercadoPagoGateway = MercadoPagoClientGateway()
+    val paymentRepository: PaymentRepository = PaymentRepositoryImpl()
+    val paymentGateway = PaymentRepositoryGateway(paymentRepository)
+    val createPaymentInteract = CreatePaymentInteract(mercadoPagoGateway, paymentGateway)
+    val updateGatewayInteract = UpdatePaymentInteract(paymentGateway)
+
     configureSerialization()
-    configurePaymentController()
+    configurePaymentController(createPaymentInteract, updateGatewayInteract)
 }
