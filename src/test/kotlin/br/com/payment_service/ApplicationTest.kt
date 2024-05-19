b6a351223d986af40c9cf34b36fe3f7ed6d4ba88
rafael.roque <rafael.roque@ifood.com.br>
2024-05-19 14:34:57 -0300

    update tests

--- src/test/kotlin/br/com/payment_service/ApplicationTest.kt
+++ src/test/kotlin/br/com/payment_service/ApplicationTest.kt
@@ -7,10 +7,10 @@ import io.ktor.server.testing.*
 import kotlin.test.*
 
 class ApplicationTest {
-    @Test
-    fun testRoot() = testApplication {
-        application {
-
-        }
-    }
+//    @Test
+//    fun testRoot() = testApplication {
+//        application {
+//
+//        }
+//    }
 }
