63bfaa0071cb1a1fdebe30e0b116492fd76419cb
dvdonadelli <davi_donadelli@live.com>
2024-05-20 13:11:13 -0300

    feat: including connection pooling and retry connection mechanism

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ val logback_version: String by project
 val postgres_version: String by project
 val exposed_version: String by project
 val koin_version: String by project
+val hikaricp_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.23"
@@ -54,6 +55,7 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
+    implementation("com.zaxxer:HikariCP:$hikaricp_version")
     implementation("io.insert-koin:koin-ktor:$koin_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
